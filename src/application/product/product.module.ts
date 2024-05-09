7217e6fb1705ba7ae5ae0535ab2f3a68bebf66e7
Jesus Daniel Gomez Hernandez <jesus.gomezher@gmail.com>
2024-05-08 21:30:54 -0500

    Feature/12: changing controller folder for inside of architecture

--- src/application/product/product.module.ts
+++ src/application/product/product.module.ts
@@ -4,7 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Product, ProductSchema } from 'src/insfrastructure/database/schemas/product.schema';
 import { ProductRepository } from 'src/application/ports/product.abstract';
 import { ProductMongoDbRepository } from 'src/insfrastructure/adapters/product.mongodb.repository';
-import { ProductController } from 'src/controllers/product/product.controller';
+import { ProductController } from 'src/insfrastructure/controllers/product/product.controller';
 
 @Module({
   imports: [
