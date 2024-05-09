2ca0695c50b07d5d5dd0bbeea7120bb61bc6d992
Jesus Daniel Gomez Hernandez <jesus.gomezher@gmail.com>
2024-05-08 21:46:39 -0500

    Feature/13-: Adding unit test for higthest sale and sales total by date

--- src/application/order/services/order.service.spec.ts
+++ src/application/order/services/order.service.spec.ts
@@ -1,7 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderService } from './order.service';
 import { OrderRepository } from 'src/application/ports/order.abstract';
-import { Order } from 'src/domain/entities/order.entity';
+import { SalesTotalForDateDto } from '../dto/sales-total-for-date.order.dto';
+import { CreateOrderDto } from '../dto/create-order.dto';
+import { Order } from 'src/insfrastructure/database/schemas/order.schema';
 
 const mockOrderRepository = {
   create: jest.fn(),
@@ -31,7 +33,7 @@ describe('OrderService', () => {
   describe('Create', () => {
     it('should create an order', async () => {
       const order: Order = {
-        orderId: 1,
+        orderId: '1',
         name: 'userName',
         total: 100,
         productList: ['product1', 'product2'],
@@ -65,7 +67,7 @@ describe('OrderService', () => {
   describe('Update', () => {
     it('should update an order', async () => {
       const order: Order = {
-        orderId: 1,
+        orderId: '1',
         name: 'userName',
         total: 100,
         productList: ['product1', 'product2'],
@@ -94,6 +96,38 @@ describe('OrderService', () => {
 
       expect(result).toBeNull();
     });
+  });
+
+  describe('highestAmountSold', () => {
+    it('should return the highest amount sold', async () => {
+      const resultExpected = new CreateOrderDto('1', 'userName', 100, ['product1', 'product2']);
+      const orderDomain = new Order('1', 'userName', 100, ['product1', 'product2'] );
+      mockOrderRepository.higthestAmountSold.mockReturnValue(orderDomain);
+
+      const result = await service.getHigthestAmountSold();
+
+      expect(result).toEqual(resultExpected);
+    });
+
+    it('should return null if there is no highest amount sold', async () => {
+      mockOrderRepository.higthestAmountSold.mockReturnValue(null);
+
+      const result = await service.getHigthestAmountSold();
+
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('getSoldOrdersByDate', () => {
+    it('should return the total sold price for a date range', async () => {
+      mockOrderRepository.getTotalSoldPrice.mockReturnValue(100);
+      const resultExpected = new SalesTotalForDateDto(100, '2021-01-01', '2021-01-02');
+
+      const result = await service.getSoldOrdersByDate('2021-01-01', '2021-01-02');
+
+      expect(result).toEqual(resultExpected);
+    });
+
   });
   
 });
